705fbad4d00e054b6494549e34c1fb1c74608ffc
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2016-08-18 23:18:11 -0400

    created capitalizeString function

--- anti-crastinate_cs.js
+++ anti-crastinate_cs.js
@@ -1,16 +1,14 @@
 // by default, chrome injects content scripts after DOM is complete
 
-// visit site
-  // get the url and the time
-    // compare time to last visit for this site
-      // if it is not passed time limit, do not permit access
-      // it it is passed time limit, register current time as last visit
+// INTIAL VARIABLE VALUES
 
 var currentUrl = document.location.host;
 var currentTime = Date.parse(new Date());
 var storage = chrome.storage.sync;
 var obj = {};
 var exploreLimit, blockLimit, lastSiteVisit;
+
+// FUNCTION DEFINITIONS
 
 function determineAccess(obj) {
 
@@ -43,16 +41,20 @@ function determineAccess(obj) {
   });
 }
 
+function capitalizeString(string) {
+  return string = string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 
 function extractDomain(url) {
   var domain;
-  // if '://' occurs, split and take third item in resulting array
+  // if 'www.' occurs, split and take third item in resulting array
   if (url.indexOf("www.") > -1) {
     domain = url.split('.')[1];
-    domain = domain.charAt(0).toUpperCase() + domain.slice(1);
+    domain = capitalizeString(domain);
   } else {
     domain = url.split('.')[0];
-    domain = domain.charAt(0).toUpperCase() + domain.slice(1);
+    domain = capitalizeString(domain);
   }
   //find & remove port number
   domain = domain.split(':')[0];
@@ -66,23 +68,9 @@ function blockSites() {
   obj[lastSiteVisit] = currentTime;
 
   determineAccess(obj);
-
-
-  // switch(currentUrl) {
-  //   case "www.facebook.com":
-  //     lastSiteVisit = 'lastFacebookVisit';
-  //     obj[lastSiteVisit] = currentTime
-
-  //     determineAccess(obj);
-  //     break;
-  //   case "twitter.com":
-  //     lastSiteVisit = 'lastTwitterVisit';
-  //     obj[lastSiteVisit] = currentTime
-
-  //     determineAccess(obj);
-  //     break;
-  // }
 }
+
+// CALL FUNCTIONS
 
 // get time limit values
 storage.get(['exploreLimit', 'blockLimit'], function(data) {
